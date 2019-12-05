8f1308edd5627787d21259f76bf6798c4f2d9343
Yipeng Sun <syp@umd.edu>
2019-12-05 06:23:32 -0500

    Add PRBS functionality.

--- dcbutil.py
+++ dcbutil.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Thu Dec 05, 2019 at 06:04 AM -0500
+# Last Change: Thu Dec 05, 2019 at 06:23 AM -0500
 
 from nanoDAQ.ut.dcb import DCB
 from nanoDAQ.utils import parse_input as proto_parse_input
@@ -46,6 +46,14 @@ GPIO status and reset.
 specify GPIO lines to reset. default to print out current value of GPIO 0-6.
     ''')
 
+    prbs_cmd = cmd.add_parser('prbs', description='''
+control PRBS register.
+''')
+    prbs_cmd.add_argument('mode',
+                          help='''
+specify the PRBS register value. on|off supported.
+    ''')
+
     return parser.parse_args()
 
 
@@ -55,11 +63,19 @@ if __name__ == '__main__':
 
     if args.cmd == 'init':
         dcb.init(args.filepath, args.slaves)
+
     elif args.cmd == 'gpio':
         if args.reset is not None:
             dcb.gpio_reset(args.reset)
         else:
             dcb.gpio_status()
+    elif args.cmd == 'prbs':
+        if args.mode == 'on':
+            dcb.write(0x1c, '03', args.slaves)
+        elif args.mode == 'off':
+            dcb.write(0x1c, '00', args.slaves)
+        else:
+            dcb.write(0x1c, int(args.mode), args.slaves)
 
     else:
         dcb.slave_status()
