ee8d98e4e6ff2dafcf230872383839163e58adb8
Yipeng Sun <syp@umd.edu>
2019-12-07 03:03:35 -0500

    Fix: Specify pydim3.

--- utils/install.sh
+++ utils/install.sh
@@ -5,6 +5,6 @@ echo "This installation only supports CentOS!"
 sudo yum install python3-devel dim-devel gcc
 
 export CPLUS_INCLUDE_PATH=/usr/local/include/dim
-pip3 install pydim "$@"
+pip3 install pydim3 "$@"
 
 pip3 install -r ../requirements.txt "$@"
