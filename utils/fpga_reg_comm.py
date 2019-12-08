8d3cc5e1a68651812dc82ca54f417abdd18809e9
Yipeng Sun <syp@umd.edu>
2019-12-08 03:11:02 -0500

    Add a elink dataframe formatter.

--- utils/fpga_reg_comm.py
+++ utils/fpga_reg_comm.py
@@ -2,11 +2,17 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Sun Dec 08, 2019 at 02:39 AM -0500
+# Last Change: Sun Dec 08, 2019 at 03:10 AM -0500
 
+import sys
 import pydim
 
 from platform import node
+
+sys.path.insert(0, '..')
+
+from nanoDAQ.utils import hex_pad
+
 
 PREFIX = 'Gbt/{}/'.format(node())
 
@@ -21,6 +27,15 @@ def srvc(name, srvc='SrvcReadings/', dev='TELL40_Dev1_0.top_tell40'):
 
 def decode(ret):
     if isinstance(ret[1], bytes):
-        return [hex(i) for i in ret[1]]
+        return [hex_pad(i) for i in ret[1]]
     elif isinstance(ret[1], str):
-        return [hex(ord(i)) for i in ret[1]]
+        return [hex_pad(ord(i)) for i in ret[1]]
+
+
+def chunks(lst, size=16):
+    return [lst[i:i + size] for i in range(0, len(lst), size)]
+
+
+def dataframe_format(df):
+    tmp_lst = map(''.join, chunks(df, 4))
+    return ' '.join(tmp_lst)
