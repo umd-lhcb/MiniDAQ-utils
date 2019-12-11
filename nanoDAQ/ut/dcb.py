191349b4e6b8ac1da2616629ac73dfdb6fb0c6df
Yipeng Sun <syp@umd.edu>
2019-12-11 17:37:50 -0500

    Fix: DCB reset.

--- nanoDAQ/ut/dcb.py
+++ nanoDAQ/ut/dcb.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Wed Dec 11, 2019 at 05:30 PM -0500
+# Last Change: Wed Dec 11, 2019 at 05:37 PM -0500
 
 import os.path as op
 
@@ -128,7 +128,7 @@ class DCB(object):
                       self.i2c_type, self.i2c_freq, data=val)
 
     def reset(self, final_state='high'):
-        self.gpio_reset(slaves=[6])
+        self.gpio_reset(chs=[6])
 
     def bias_cur_status(self, slaves=None, output=True):
         self.activate_i2c()
