a047398b6b71bf8156f94f43a0ab00ffd7eab5a6
Yipeng Sun <syp@umd.edu>
2019-12-16 02:55:16 -0500

    Wrap FPGA communications in another process.

--- utils/fpga_reg_comm.py
+++ utils/fpga_reg_comm.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Mon Dec 09, 2019 at 02:00 AM -0500
+# Last Change: Mon Dec 16, 2019 at 02:55 AM -0500
 
 import sys
 
@@ -13,11 +13,12 @@ from nanoDAQ.gbtclient.fpga_reg import mem_mon_fiber_read, mem_mon_fiber_write
 from nanoDAQ.gbtclient.fpga_reg import mem_mon_options_read, \
     mem_mon_options_write
 from nanoDAQ.phase import print_elink_table, alternating_color
+from nanoDAQ.utils import exec_guard
 
 
-read = mem_mon_read
-fiber_r = mem_mon_fiber_read
-fiber_w = mem_mon_fiber_write
-opts_r = mem_mon_options_read
-opts_w = mem_mon_options_write
-tab = lambda x: print_elink_table(x, style=alternating_color)
+read    = lambda: exec_guard(mem_mon_read())
+fiber_r = lambda: exec_guard(mem_mon_fiber_read())
+fiber_w = lambda x: exec_guard(mem_mon_fiber_write(x))
+opts_r  = lambda: exec_guard(mem_mon_options_read)
+opts_w  = lambda: exec_guard(mem_mon_options_write())
+tab     = lambda x: print_elink_table(x, style=alternating_color)
