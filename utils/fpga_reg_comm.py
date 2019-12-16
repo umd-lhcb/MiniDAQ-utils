4019cf7990d25178d65b50f5c945c4514a2d29ad
Yipeng Sun <syp@umd.edu>
2019-12-16 12:07:11 -0500

    Fix: Correct wrapping.

--- utils/fpga_reg_comm.py
+++ utils/fpga_reg_comm.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Mon Dec 16, 2019 at 02:55 AM -0500
+# Last Change: Mon Dec 16, 2019 at 12:07 PM -0500
 
 import sys
 
@@ -16,9 +16,9 @@ from nanoDAQ.phase import print_elink_table, alternating_color
 from nanoDAQ.utils import exec_guard
 
 
-read    = lambda: exec_guard(mem_mon_read())
-fiber_r = lambda: exec_guard(mem_mon_fiber_read())
-fiber_w = lambda x: exec_guard(mem_mon_fiber_write(x))
+read    = lambda: exec_guard(mem_mon_read)
+fiber_r = lambda: exec_guard(mem_mon_fiber_read)
+fiber_w = lambda x: exec_guard(mem_mon_fiber_write, x)
 opts_r  = lambda: exec_guard(mem_mon_options_read)
-opts_w  = lambda: exec_guard(mem_mon_options_write())
+opts_w  = lambda: exec_guard(mem_mon_options_write)
 tab     = lambda x: print_elink_table(x, style=alternating_color)
