c203ede92d26ae15fe8116b1c4363eda3e006da2
Yipeng Sun <syp@umd.edu>
2019-12-18 03:36:36 -0500

    Initialize DCB and SALT.

--- phaseadj.py
+++ phaseadj.py
@@ -2,9 +2,12 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Wed Dec 18, 2019 at 02:42 AM -0500
+# Last Change: Wed Dec 18, 2019 at 02:48 AM -0500
 
 from argparse import ArgumentParser
+
+from nanoDAQ.ut.dcb import DCB
+from nanoDAQ.ut.salt import SALT
 
 from nanoDAQ.gbtclient.fpga_reg import mem_mon_read_safe as mem_r
 from nanoDAQ.gbtclient.fpga_reg import mem_mon_fiber_write_safe as fiber_w
@@ -53,6 +56,10 @@ specify ASIC.
 if __name__ == '__main__':
     parser = parse_input()
     args = parser.parse_args()
+
+    # Initialize devices
+    dcb = DCB(args.gbt)
+    salt = SALT(args.gbt, args.bus)
 
     # Enable memory monitoring options (looping, etc.)
     opts_w()
