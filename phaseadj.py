e72212512f71a008819904eeba55c03983c443aa
Yipeng Sun <syp@umd.edu>
2019-12-19 05:29:58 -0500

    Alignment.

--- phaseadj.py
+++ phaseadj.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Thu Dec 19, 2019 at 02:51 AM -0500
+# Last Change: Thu Dec 19, 2019 at 05:29 AM -0500
 
 from argparse import ArgumentParser
 from tabulate import tabulate
@@ -85,4 +85,5 @@ if __name__ == '__main__':
     print('Generating phase-scanning table...')
     phase_scan_raw = loop_through_elink_phase(args.gbt, args.slave, daq_chs)
     phase_scan_tab = check_phase_scan(phase_scan_raw)
-    print(tabulate(phase_scan_tab, headers=['phase']+daq_chs))
+    print(tabulate(phase_scan_tab, headers=['phase']+daq_chs),
+          alignment=['left', 'right', 'right', 'right'])
