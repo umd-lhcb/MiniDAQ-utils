b5b1d16303dfddf42400f458a56792ea9b390401
Yipeng Sun <syp@umd.edu>
2020-01-17 04:30:59 -0500

    Add the missing flag.

--- dcbelk.sh
+++ dcbelk.sh
@@ -2,14 +2,24 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Fri Jan 17, 2020 at 04:26 AM -0500
+# Last Change: Fri Jan 17, 2020 at 04:30 AM -0500
 
 GBT=0
+
 declare -A ELK_CHS=(
     [3]="11 10 9"
     [2]="8 7 6"
     [1]="5 4 3"
     [0]="2 1 0"
+)
+
+declare -A MINIDAQ_CHS=(
+    [1]=22
+    [2]=12
+    [3]=23
+    [4]=21
+    [5]=14
+    [6]=13
 )
 
 case $1 in
@@ -65,6 +75,7 @@ case $1 in
             for asic in 3 2 1; do
                 ./phaseadj.py -g $GBT -s $gbtx \
                     -b ${I2C_BUS[$gbtx]} -a $asic \
+                    -c ${MINIDAQ_CHS[$gbtx]} \
                     -e $(echo ${ELK_CHS[$asic]}) \
                     --adjust-elink-phase y \
                     --adjust-tfc-phase n \
@@ -73,6 +84,7 @@ case $1 in
             # Print out the final result for each GBTx
             ./phaseadj.py -g $GBT -s $gbtx \
                 -b ${I2C_BUS[$gbtx]} -a 0 \
+                -c ${MINIDAQ_CHS[$gbtx]} \
                 -e $(echo ${ELK_CHS[0]}) \
                 --adjust-elink-phase y \
                 --adjust-tfc-phase n
