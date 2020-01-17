f72c2030d795953e54b6e7f9bbbd201d9885c609
Yipeng Sun <syp@umd.edu>
2020-01-17 04:11:25 -0500

    Various fixes for the DCB tester.

--- elkadj.sh
+++ elkadj.sh
@@ -2,21 +2,21 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Fri Jan 17, 2020 at 03:58 AM -0500
+# Last Change: Fri Jan 17, 2020 at 04:10 AM -0500
 
 GBT=0
 declare -A ELK_CHS=(
-    [3]="11 10 9"
-    [2]="8 7 6"
-    [1]="5 4 3"
-    [0]="2 1 0"
+    [3]=( 11 10 9 )
+    [2]=( 8 7 6 )
+    [1]=( 5 4 3 )
+    [0]=( 2 1 0 )
 )
 
 case $1 in
-    FH) # First half of the DCB
-        # On Mirror BP, we are using JP9 and JD10
+    FH) # On Mirror BP, we are using JP9 and JD10
         ./dcbutil.py init ./gbtx_config/slave-Tx-wrong_termination.txt -g $GBT
 
+        GBTXS=( 3 4 5 )
         declare -A I2C_BUS=(
             [3]=2
             [4]=1
@@ -28,7 +28,8 @@ case $1 in
         done
 
         # Now we start adjusting phase
-        for gbtx in 3 4 5; do
+        for gbtx in ${GBTXS[@]}; do
+            echo "Validating GBTx $gbtx..."
             for asic in 3 2 1; do
                 ./phaseadj.py -g $GBT -s $gbtx \
                     -b ${I2C_BUS[$gbtx]} -a $asic -e ${ELK_CHS[$asic]} \
@@ -39,6 +40,38 @@ case $1 in
             # Print out the final result for each GBTx
             ./phaseadj.py -g $GBT -s $gbtx \
                 -b ${I2C_BUS[$gbtx]} -a 0 -e ${ELK_CHS[0]} \
+                --adjust-elink-phase y \
+                --adjust-tfc-phase n
+        done
+        ;;
+
+    SH) # On Mirror BP, we are using JP10 and JD10
+        ./dcbutil.py init ./gbtx_config/slave-Tx-wrong_termination.txt -g $GBT
+
+        GBTXS=( 1 2 6 )
+        declare -A I2C_BUS=(
+            [1]=4
+            [2]=3
+            [6]=5
+        )
+
+        for bus in ${I2C_BUS[@]}; do
+            ./saltutil.py $bus init -g $GBT
+        done
+
+        # Now we start adjusting phase
+        for gbtx in ${GBTXS[@]}; do
+            echo "Validating GBTx $gbtx..."
+            for asic in 3 2 1; do
+                ./phaseadj.py -g $GBT -s $gbtx \
+                    -b ${I2C_BUS[$gbtx]} -a $asic -e ${ELK_CHS[$asic][@]} \
+                    --adjust-elink-phase y \
+                    --adjust-tfc-phase n \
+                    --non-verbose
+            done
+            # Print out the final result for each GBTx
+            ./phaseadj.py -g $GBT -s $gbtx \
+                -b ${I2C_BUS[$gbtx]} -a 0 -e ${ELK_CHS[0][@]} \
                 --adjust-elink-phase y \
                 --adjust-tfc-phase n
         done
