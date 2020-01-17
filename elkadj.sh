335d7d27cdb8467a4c7855953d872a6976151057
Yipeng Sun <syp@umd.edu>
2020-01-17 03:58:48 -0500

    Add a script to automate the DCB testing.

---
+++ elkadj.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#
+# Author: Yipeng Sun
+# License: BSD 2-clause
+# Last Change: Fri Jan 17, 2020 at 03:58 AM -0500
+
+GBT=0
+declare -A ELK_CHS=(
+    [3]="11 10 9"
+    [2]="8 7 6"
+    [1]="5 4 3"
+    [0]="2 1 0"
+)
+
+case $1 in
+    FH) # First half of the DCB
+        # On Mirror BP, we are using JP9 and JD10
+        ./dcbutil.py init ./gbtx_config/slave-Tx-wrong_termination.txt -g $GBT
+
+        declare -A I2C_BUS=(
+            [3]=2
+            [4]=1
+            [5]=0
+        )
+
+        for bus in ${I2C_BUS[@]}; do
+            ./saltutil.py $bus init -g $GBT
+        done
+
+        # Now we start adjusting phase
+        for gbtx in 3 4 5; do
+            for asic in 3 2 1; do
+                ./phaseadj.py -g $GBT -s $gbtx \
+                    -b ${I2C_BUS[$gbtx]} -a $asic -e ${ELK_CHS[$asic]} \
+                    --adjust-elink-phase y \
+                    --adjust-tfc-phase n \
+                    --non-verbose
+            done
+            # Print out the final result for each GBTx
+            ./phaseadj.py -g $GBT -s $gbtx \
+                -b ${I2C_BUS[$gbtx]} -a 0 -e ${ELK_CHS[0]} \
+                --adjust-elink-phase y \
+                --adjust-tfc-phase n
+        done
+        ;;
+
+    *)
+        echo "Unknown option: $1."
+        ;;
+
+esac
